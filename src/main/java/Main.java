ecaf44677292066e97001fd3bb01d84ab43810a0
lorris saint-genez <lorris.saint-genez@epita.fr>
2017-06-10 22:19:04 +0200

    Dynamic class loader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,83 +1,111 @@
-import Ex2.Entity.*;
 import Ex2.Service.ServiceGeneric;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
 
+import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.net.URL;
+import java.util.*;
 
 /**
  * Created by Lorris on 10/06/2017.
  */
 public class Main {
 
+    private static List<Class<?>> findInPackage(String packageName) {
+        String path = packageName.replace('.', '/');
+        URL url = Thread.currentThread().getContextClassLoader().getResource(path);
+        if (url == null)
+            throw new IllegalArgumentException("Unable to get resources");
+        File scannedDir = new File(url.getFile());
+        List<Class<?>> classes = new ArrayList<>();
+        for (File f : scannedDir.listFiles())
+            classes.addAll(find(f, packageName));
+        return classes;
+    }
+
+    private static List<Class<?>> find(File file, String scannedPackage) {
+        List<Class<?>> classes = new ArrayList<>();
+        String resource = scannedPackage + '.' + file.getName();
+        if (file.isDirectory()) {
+            for (File child : file.listFiles()) {
+                classes.addAll(find(child, resource));
+            }
+        } else if (resource.endsWith(".class")) {
+            int endIndex = resource.length() - ".class".length();
+            String className = resource.substring(0, endIndex);
+            try {
+                classes.add(Class.forName(className));
+            } catch (ClassNotFoundException ignore) {
+            }
+        }
+        return classes;
+    }
+
+    private static void AddEntity(String[] input, Class cls, ServiceGeneric serviceGeneric) {
+        Object[] vars = new Object[input.length - 2];
+        Class[] classes = new Class[input.length - 2];
+
+        for (int i = 2; i < input.length; i++) {
+            vars[i - 2] = input[i];
+            classes[i - 2] = input[i].getClass();
+        }
+
+        try {
+            Constructor<?> cTor = cls.getConstructor(classes);
+            serviceGeneric.createEntity(cTor.newInstance(vars));
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
 
+        final List<Class<?>> classesForPackage = findInPackage("Ex2.Entity");
         Map<String, Class<?>> classMap = new HashMap<>();
 
-        classMap.put("user", User.class);
-        classMap.put("file", File.class);
-        classMap.put("media", Media.class);
-        classMap.put("parameter", Parameter.class);
-        classMap.put("profile", Profile.class);
-        classMap.put("sharing", Sharing.class);
-        classMap.put("value", Value.class);
-        classMap.put("php", ProfileHasParameter.class);
+        classesForPackage.forEach(c -> classMap.put(c.getSimpleName().toLowerCase(), c));
 
-        ServiceGeneric serviceGeneric = new ServiceGeneric();
+        final ServiceGeneric serviceGeneric = new ServiceGeneric();
 
         while(true) {
             Scanner reader = new Scanner(System.in);
-            System.out.println("Write an action : ");
+            System.out.println("You can : ");
+            System.out.println("list-\'model\' : Will list all entity of the requested model");
+            System.out.println("list-\'model\'-json : will list all entity of the requested model (JSON Format)");
+            System.out.println("add-\'model\' $param... : will create an entity of the model if all params fit the constructor");
+            System.out.println("exit : Will exit the program");
+            System.out.print("Choose your action : ");
             String[] input = reader.nextLine().split("[-\\s]");
             String cmd = input[0].toLowerCase();
-            String classAction = input[1].toLowerCase();
-            Class cls = classMap.get(classAction);
             switch (cmd) {
                 case "list":
+                    String classAction = input[1].toLowerCase();
+                    Class cls = classMap.get(classAction);
                     try {
-                        String jsonAction = input[2].toLowerCase();
-                        ArrayList listEntity = serviceGeneric.listEntity(cls);
-                        StringBuilder sb = new StringBuilder();
-                        listEntity.forEach(e -> {
-                            ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-                            try {
-                                sb.append(ow.writeValueAsString(e));
-                            } catch (IOException ioe) {
-                                ioe.printStackTrace();
-                            }
-                        });
-                        System.out.println(sb.toString());
+                        if (input[2].toLowerCase().equals("json")) {
+                            ArrayList listEntity = serviceGeneric.listEntity(cls);
+                            StringBuilder sb = new StringBuilder();
+                            listEntity.forEach(e -> {
+                                ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+                                try {
+                                    sb.append(ow.writeValueAsString(e));
+                                } catch (IOException ioe) {
+                                    ioe.printStackTrace();
+                                }
+                            });
+                            System.out.println(sb.toString());
+                        }
                     } catch (Exception e) {
                         serviceGeneric.listEntity(cls).forEach(System.out::println);
                     }
                     break;
                 case "add":
-                    Object[] vars = new Object[input.length - 2];
-                    Class[] classes = new Class[input.length - 2];
-
-                    for (int i = 2; i < input.length; i++) {
-                        vars[i - 2] = input[i];
-                        classes[i - 2] = input[i].getClass();
-                    }
-
-                    try {
-                        Constructor cTor = null;
-                        try {
-                            cTor = cls.getConstructor(classes);
-                            serviceGeneric.createEntity(cTor.newInstance(vars));
-                        } catch (NoSuchMethodException e) {
-                            e.printStackTrace();
-                        }
-                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-                        e.printStackTrace();
-                    }
+                    String classString = input[1].toLowerCase();
+                    Class classFromMap = classMap.get(classString);
+                    AddEntity(input, classFromMap, serviceGeneric);
                     break;
                 case "exit":
                     System.exit(0);
@@ -85,54 +113,7 @@ public class Main {
                 default:
                     break;
             }
+            System.out.println("============= DONE =============");
         }
-
-        /*Class cls = classMap.get("user");
-        serviceGeneric.listEntity(cls).forEach(System.out::println);*/
-
-        /*File file = new File();
-        file.setFormat("json");
-        file.setPath("/bin/lib");
-        File file2 = new File();
-        file2.setPath("/game/aoe");
-        file2.setFormat("exe");
-
-        em.persist(file);
-        em.persist(file2);
-
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Pick an action : ");
-        String input = reader.next();
-        String cmd = input.toLowerCase().split("-")[0];
-        switch (cmd) {
-            case "list":
-                if (input.split("-")[2].equals("json")) {
-                    ArrayList<Student> students = listEntity(em, Student.class);
-                    students.forEach((s) -> {
-                        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-                        try {
-                            ow.writeValue(System.out, s);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
-                } else {
-                    ArrayList<File> files = listEntity(em, File.class);
-                    files.forEach(System.out::println);
-                }
-                break;
-            case "add":
-                System.out.println("Enter : login, password, firstname, lastname and email");
-                em.getTransaction().begin();
-                Student student = createStudent(reader.next().trim(), reader.next().trim(), reader.next().trim(), reader.next().trim(), reader.next().trim());
-                em.persist(student);
-                em.getTransaction().commit();
-                break;
-            default:
-                break;
-        }
-
-        em.close();
-        emf.close();*/
     }
 }
